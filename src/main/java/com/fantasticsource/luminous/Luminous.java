c432f8e437520384ee4bd3989464ca70870cc417
Kyle Koder <silvertallon@gmail.com>
2020-09-03 20:01:57 -0700

    Basic tweaked class test seems to be working

--- src/main/java/com/fantasticsource/luminous/Luminous.java
+++ src/main/java/com/fantasticsource/luminous/Luminous.java
@@ -1,5 +1,7 @@
 package com.fantasticsource.luminous;
 
+import com.fantasticsource.mctools.MCTools;
+import net.minecraft.launchwrapper.LaunchClassLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
@@ -8,12 +10,21 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = Luminous.MODID, name = Luminous.NAME, version = Luminous.VERSION)
+@Mod(modid = Luminous.MODID, name = Luminous.NAME, version = Luminous.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.036,)")
 public class Luminous
 {
     public static final String MODID = "luminous";
     public static final String NAME = "Luminous";
     public static final String VERSION = "1.12.2.000";
+
+    static
+    {
+        if (!MCTools.devEnv())
+        {
+            LaunchClassLoader classLoader = (LaunchClassLoader) Luminous.class.getClassLoader();
+            classLoader.registerTransformer("com.fantasticsource.luminous.LuminousTransformer");
+        }
+    }
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
@@ -26,4 +37,18 @@ public class Luminous
     {
         if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
     }
+
+//    @SubscribeEvent
+//    public static void test(InventoryChangedEvent event)
+//    {
+//        Entity entity = event.getEntity();
+//        ItemStack stack = GlobalInventory.getVanillaMainhandItem(entity);
+//        if (stack == null) return;
+//
+//        if (stack.isEmpty())
+//        {
+//            BlockPos pos = entity.getPosition();
+//            entity.world.getLightFromNeighbors()
+//        }
+//    }
 }
---
+++ src/main/java/com/fantasticsource/luminous/LuminousTransformer.java
@@ -0,0 +1,45 @@
+package com.fantasticsource.luminous;
+
+import com.fantasticsource.mctools.MCTools;
+import net.minecraft.launchwrapper.IClassTransformer;
+import net.minecraft.util.text.TextFormatting;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.fantasticsource.luminous.Luminous.MODID;
+
+public class LuminousTransformer implements IClassTransformer
+{
+    private static final ArrayList<String> REPLACEMENTS = new ArrayList<>(Arrays.asList(
+            "net.minecraft.world.chunk.storage.ExtendedBlockStorage"
+    ));
+
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] bytes)
+    {
+        if (REPLACEMENTS.contains(transformedName))
+        {
+            String name2 = MODID + "/tweakedclasses/" + transformedName.replaceAll("[.]()", "/") + ".class";
+            InputStream stream = MCTools.getJarResourceStream(LuminousTransformer.class, name2);
+            if (stream == null)
+            {
+                System.err.println(TextFormatting.RED + "Resource not found: " + name2);
+                return bytes;
+            }
+            byte[] buf = new byte[10000];
+            try
+            {
+                bytes = new byte[stream.read(buf)];
+                System.arraycopy(buf, 0, bytes, 0, bytes.length);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return bytes;
+    }
+}

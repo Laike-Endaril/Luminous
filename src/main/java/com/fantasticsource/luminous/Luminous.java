ec6cb31013d0ddbd43a5c42b652a4c81425e6021
Kyle Koder <silvertallon@gmail.com>
2020-09-05 23:24:38 -0700

    Overrides are now saved / loaded correctly and server always seems to be good, but client has a couple sync issues (see TODO)

--- src/main/java/com/fantasticsource/luminous/Luminous.java
+++ src/main/java/com/fantasticsource/luminous/Luminous.java
@@ -1,10 +1,12 @@
 package com.fantasticsource.luminous;
 
+import com.fantasticsource.fantasticlib.api.FLibAPI;
 import com.fantasticsource.mctools.MCTools;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.management.PlayerChunkMapEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.EnumSkyBlock;
@@ -15,6 +17,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -22,7 +25,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.LinkedHashMap;
 
-@Mod(modid = Luminous.MODID, name = Luminous.NAME, version = Luminous.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.036,)")
+@Mod(modid = Luminous.MODID, name = Luminous.NAME, version = Luminous.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.036x,)")
 public class Luminous
 {
     public static final String MODID = "luminous";
@@ -43,6 +46,7 @@ public class Luminous
     {
         Network.init();
         MinecraftForge.EVENT_BUS.register(Luminous.class);
+        FLibAPI.attachNBTCapToWorldIf(MODID, o -> o instanceof WorldServer);
     }
 
     @SubscribeEvent
@@ -79,7 +83,7 @@ public class Luminous
     }
 
 
-    protected static void updateLight(World world, Chunk centerChunk, BlockPos pos, EnumSkyBlock type, Integer light)
+    protected static void updateLight(World world, Chunk chunk, BlockPos pos, EnumSkyBlock type, Integer light)
     {
         //Force local light update
         for (BlockPos involved : new BlockPos[]{pos, pos.up(), pos.down(), pos.north(), pos.south(), pos.west(), pos.east()})
@@ -88,10 +92,14 @@ public class Luminous
         }
 
 
-        //If server, forward light update packet to tracking clients
+        //If server...
         if (world instanceof WorldServer)
         {
-            PlayerChunkMapEntry playerChunkMapEntry = ((WorldServer) world).getPlayerChunkMap().getEntry(centerChunk.x, centerChunk.z);
+            //Save persistent override data
+            updateLightOverrideNBTCap((WorldServer) world, chunk, pos, type, light);
+
+            //Sync to client
+            PlayerChunkMapEntry playerChunkMapEntry = ((WorldServer) world).getPlayerChunkMap().getEntry(chunk.x, chunk.z);
             if (playerChunkMapEntry != null)
             {
                 for (EntityPlayerMP player : playerChunkMapEntry.getWatchingPlayers())
@@ -112,5 +120,54 @@ public class Luminous
         WorldServer world = (WorldServer) entity.world;
         BlockPos pos = entity.getPosition().down();
         if (!setLightOverride(world, pos, EnumSkyBlock.BLOCK, 15)) setLightOverride(world, pos, EnumSkyBlock.BLOCK, null);
+    }
+
+
+    protected static void updateLightOverrideNBTCap(WorldServer world, Chunk chunk, BlockPos pos, EnumSkyBlock type, Integer light)
+    {
+        NBTTagCompound compound = FLibAPI.getNBTCap(world).getCompound(MODID);
+        if (light == null) MCTools.removeSubNBTAndClean(compound, "" + chunk.x, "" + chunk.z, type.name(), "" + pos.getY(), "" + pos.getX(), "" + pos.getZ());
+        else
+        {
+            compound = MCTools.getOrGenerateSubCompound(compound, "" + chunk.x, "" + chunk.z, type.name(), "" + pos.getY(), "" + pos.getX());
+            compound.setInteger("" + pos.getZ(), light);
+        }
+    }
+
+    @SubscribeEvent
+    protected static void loadChunk(ChunkEvent.Load event)
+    {
+        World world = event.getWorld();
+        if (!(world instanceof WorldServer)) return;
+
+        Chunk chunk = event.getChunk();
+        NBTTagCompound compound = MCTools.getSubCompoundIfExists(FLibAPI.getNBTCap(world).getCompound(MODID), "" + chunk.x, "" + chunk.z);
+        if (compound == null) return;
+
+
+        for (String typeString : compound.getKeySet())
+        {
+            EnumSkyBlock type = EnumSkyBlock.valueOf(typeString);
+            NBTTagCompound typeCompound = compound.getCompoundTag(typeString);
+
+            for (String yString : typeCompound.getKeySet())
+            {
+                int y = Integer.parseInt(yString);
+                NBTTagCompound yCompound = typeCompound.getCompoundTag(yString);
+
+                for (String xString : yCompound.getKeySet())
+                {
+                    int x = Integer.parseInt(xString);
+                    NBTTagCompound xCompound = yCompound.getCompoundTag(xString);
+
+                    for (String zString : xCompound.getKeySet())
+                    {
+                        int z = Integer.parseInt(zString);
+                        int light = xCompound.getInteger(zString);
+                        setLightOverride(world, new BlockPos(x, y, z), type, light);
+                    }
+                }
+            }
+        }
     }
 }

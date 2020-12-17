9ef1415b50ee50bac42fe29613c32d46f203ffde
Kyle Koder <silvertallon@gmail.com>
2020-12-16 18:20:55 -0800

    Removed unused classes (for this branch)

--- src/main/java/com/fantasticsource/luminous/Luminous.java
+++ src/main/java/com/fantasticsource/luminous/Luminous.java
@@ -1,17 +1,10 @@
 package com.fantasticsource.luminous;
 
-import com.fantasticsource.luminous.asm.ClassMapper;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 
 import static com.fantasticsource.luminous.asm.LuminousCore.*;
 
 @Mod(modid = MODID, name = NAME, version = VERSION)
 public class Luminous
 {
-    @Mod.EventHandler
-    public static void temporary(FMLServerStoppedEvent event)
-    {
-        ClassMapper.save();
-    }
 }

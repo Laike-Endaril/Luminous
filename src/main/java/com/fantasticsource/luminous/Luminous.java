efcd50bf6fd013afc396859d4d1617e0fd9d3625
Laike-Endaril <silvertallon@gmail.com>
2025-03-20 14:08:00 -0700

    Basic test; mod functions and replaces World.getRawLight() correctly in both IDE and as standalone mod.  Currently just makes it return 0 as a test

--- src/main/java/com/fantasticsource/luminous/Luminous.java
+++ src/main/java/com/fantasticsource/luminous/Luminous.java
@@ -32,12 +32,12 @@ public class Luminous
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
     {
-        Network.init();
+//        Network.init();
         MinecraftForge.EVENT_BUS.register(Luminous.class);
-        MinecraftForge.EVENT_BUS.register(Light.class);
-        FLibAPI.attachNBTCapToWorldIf(MODID, o -> o instanceof WorldServer);
-        MinecraftForge.EVENT_BUS.register(LightDataHandler.class);
-        MinecraftForge.EVENT_BUS.register(LightHandler.class);
+//        MinecraftForge.EVENT_BUS.register(Light.class);
+//        FLibAPI.attachNBTCapToWorldIf(MODID, o -> o instanceof WorldServer);
+//        MinecraftForge.EVENT_BUS.register(LightDataHandler.class);
+//        MinecraftForge.EVENT_BUS.register(LightHandler.class);
     }
 
     @SubscribeEvent
@@ -47,63 +47,63 @@ public class Luminous
     }
 
 
-    @SubscribeEvent
-    public static void staticLightTest(EntityJoinWorldEvent event)
-    {
-        Entity entity = event.getEntity();
-        if (entity.world.isRemote || !(entity instanceof EntitySnowball)) return;
-
-        WorldServer world = (WorldServer) entity.world;
-
-        world.profiler.startSection(NAME + ": staticLightTest");
-
-        BlockPos pos = entity.getPosition().down();
-        if (LightDataHandler.setModdedLight(world, pos, MODID, "snow", 7) != 0)
-        {
-            LightDataHandler.setModdedLight(world, pos, MODID, "snow", 0);
-        }
-
-        world.profiler.endSection();
-    }
-
-
-    protected static final LinkedHashMap<EntityLivingBase, WorldServer> LIT_WORLDS = new LinkedHashMap<>();
-    protected static final LinkedHashMap<EntityLivingBase, BlockPos> LIT_POSITIONS = new LinkedHashMap<>();
-
-    @SubscribeEvent
-    public static void movingLightTest(LivingEvent.LivingUpdateEvent event)
-    {
-        EntityLivingBase livingBase = event.getEntityLiving();
-        if (livingBase.world.isRemote) return;
-
-
-        WorldServer world = (WorldServer) livingBase.world, litWorld = LIT_WORLDS.get(livingBase);
-
-        world.profiler.startSection(NAME + ": movingLightTest");
-
-        BlockPos eyePos = new BlockPos(livingBase.getPositionEyes(0)), litPosition = LIT_POSITIONS.get(livingBase);
-
-        if (world != litWorld || !eyePos.equals(litPosition))
-        {
-            LightDataHandler.setModdedLight(world, eyePos, MODID, "" + livingBase.getUniqueID(), 15);
-            if (litWorld != null) LightDataHandler.setModdedLight(litWorld, litPosition, MODID, "" + livingBase.getUniqueID(), 0);
-            LIT_WORLDS.put(livingBase, world);
-            LIT_POSITIONS.put(livingBase, eyePos);
-        }
-
-        world.profiler.endSection();
-    }
-
-
-    @SubscribeEvent
-    public static void lightsTest(PlayerInteractEvent.EntityInteractSpecific event)
-    {
-        EntityPlayer player = event.getEntityPlayer();
-        if (!(player instanceof EntityPlayerMP) || event.getHand() != EnumHand.MAIN_HAND) return;
-
-        if (!LightHandler.addMovingLightToEntity(MODID, player, 15))
-        {
-            LightHandler.removeMovingLightFromEntity(MODID, player);
-        }
-    }
+//    @SubscribeEvent
+//    public static void staticLightTest(EntityJoinWorldEvent event)
+//    {
+//        Entity entity = event.getEntity();
+//        if (entity.world.isRemote || !(entity instanceof EntitySnowball)) return;
+//
+//        WorldServer world = (WorldServer) entity.world;
+//
+//        world.profiler.startSection(NAME + ": staticLightTest");
+//
+//        BlockPos pos = entity.getPosition().down();
+//        if (LightDataHandler.setModdedLight(world, pos, MODID, "snow", 7) != 0)
+//        {
+//            LightDataHandler.setModdedLight(world, pos, MODID, "snow", 0);
+//        }
+//
+//        world.profiler.endSection();
+//    }
+//
+//
+//    protected static final LinkedHashMap<EntityLivingBase, WorldServer> LIT_WORLDS = new LinkedHashMap<>();
+//    protected static final LinkedHashMap<EntityLivingBase, BlockPos> LIT_POSITIONS = new LinkedHashMap<>();
+//
+//    @SubscribeEvent
+//    public static void movingLightTest(LivingEvent.LivingUpdateEvent event)
+//    {
+//        EntityLivingBase livingBase = event.getEntityLiving();
+//        if (livingBase.world.isRemote) return;
+//
+//
+//        WorldServer world = (WorldServer) livingBase.world, litWorld = LIT_WORLDS.get(livingBase);
+//
+//        world.profiler.startSection(NAME + ": movingLightTest");
+//
+//        BlockPos eyePos = new BlockPos(livingBase.getPositionEyes(0)), litPosition = LIT_POSITIONS.get(livingBase);
+//
+//        if (world != litWorld || !eyePos.equals(litPosition))
+//        {
+//            LightDataHandler.setModdedLight(world, eyePos, MODID, "" + livingBase.getUniqueID(), 15);
+//            if (litWorld != null) LightDataHandler.setModdedLight(litWorld, litPosition, MODID, "" + livingBase.getUniqueID(), 0);
+//            LIT_WORLDS.put(livingBase, world);
+//            LIT_POSITIONS.put(livingBase, eyePos);
+//        }
+//
+//        world.profiler.endSection();
+//    }
+//
+//
+//    @SubscribeEvent
+//    public static void lightsTest(PlayerInteractEvent.EntityInteractSpecific event)
+//    {
+//        EntityPlayer player = event.getEntityPlayer();
+//        if (!(player instanceof EntityPlayerMP) || event.getHand() != EnumHand.MAIN_HAND) return;
+//
+//        if (!LightHandler.addMovingLightToEntity(MODID, player, 15))
+//        {
+//            LightHandler.removeMovingLightFromEntity(MODID, player);
+//        }
+//    }
 }
--- src/main/java/com/fantasticsource/luminous/asm/LuminousTransformer.java
+++ src/main/java/com/fantasticsource/luminous/asm/LuminousTransformer.java
@@ -1,29 +1,93 @@
 package com.fantasticsource.luminous.asm;
 
-import com.fantasticsource.luminous.asm.classtransformers.BlockStateContainerDump;
-import com.fantasticsource.luminous.asm.classtransformers.ChunkDump;
-import com.fantasticsource.luminous.asm.classtransformers.WorldDump;
 import net.minecraft.launchwrapper.IClassTransformer;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.*;
+
+import java.util.ArrayList;
+
+import static org.objectweb.asm.Opcodes.ALOAD;
+import static org.objectweb.asm.Opcodes.INVOKESTATIC;
 
 public class LuminousTransformer implements IClassTransformer
 {
-    @Override
-    public byte[] transform(String name, String transformedName, byte[] bytes)
+    private static final ArrayList<String> CLASSES_TO_TRANSFORM = new ArrayList<>();
+
+    static
     {
-        if (name.equals(transformedName)) return bytes;
+        CLASSES_TO_TRANSFORM.add("net.minecraft.world.World");
+        CLASSES_TO_TRANSFORM.add("amu");
+    }
 
-        switch (transformedName)
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] classBytes)
+    {
+        if (CLASSES_TO_TRANSFORM.contains(name)) return transform(name, classBytes);
+        return classBytes;
+    }
+
+
+    private static byte[] transform(String name, byte[] classBytes)
+    {
+        System.out.println("Transforming " + name);
+        try
         {
-            case "net.minecraft.world.World":
-                return WorldDump.dump();
+            ClassNode classNode = new ClassNode();
+            ClassReader classReader = new ClassReader(classBytes);
+            classReader.accept(classNode, 0);
 
-            case "net.minecraft.world.chunk.Chunk":
-                return ChunkDump.dump();
 
-            case "net.minecraft.block.state.BlockStateContainer":
-                return BlockStateContainerDump.dump();
+            //When adding new instructions to existing methods, it's best to add them at the start if possible to avoid frame-related issues
+            switch (name)
+            {
+                case "net.minecraft.world.World":
+                case "amu":
+//                    ASMHelper.ASMify(name);
+
+
+                    for (MethodNode methodNode : classNode.methods)
+                    {
+//                        System.out.println(methodNode.name);
+//                        System.out.println(methodNode.desc);
+                        for (AbstractInsnNode instructionNode : methodNode.instructions.toArray())
+                        {
+                            if (instructionNode instanceof MethodInsnNode)
+                            {
+                                MethodInsnNode methodInstruction = (MethodInsnNode) instructionNode;
+                                //When in obfuscated mode (normal run conditions), one class can have a ton of methods named "a", so use desc instead
+                                if (methodInstruction.desc.equals("(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/EnumSkyBlock;)I") || methodInstruction.desc.equals("(Let;Lana;)I")) //getRawLight
+                                {
+//                                    System.out.println(methodInstruction.name);
+//                                    System.out.println(methodInstruction.desc);
+
+                                    AbstractInsnNode otherNode = methodInstruction.getPrevious();
+                                    while (!(otherNode instanceof VarInsnNode) || otherNode.getOpcode() != ALOAD || ((VarInsnNode) otherNode).var != 0)
+                                    {
+                                        otherNode = otherNode.getPrevious();
+                                    }
+                                    methodNode.instructions.remove(otherNode);
+                                    methodNode.instructions.set(instructionNode, new MethodInsnNode(INVOKESTATIC, "com/fantasticsource/luminous/MethodRedirectTest", "worldGetRawLightRedirect", "(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/EnumSkyBlock;)I", false));
+                                }
+//                                else System.out.println(methodInstruction.name);
+                            }
+                        }
+                    }
+
+
+//                    transformChunk(name, classNode, obfuscated);
+                    break;
+            }
+
+
+            ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS); //COMPUTE_FRAMES has major issues and should not be used; any necessary frame alterations should be handled manually (unfortunately)
+            classNode.accept(classWriter);
+            return classWriter.toByteArray();
         }
-
-        return bytes;
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return classBytes;
     }
 }

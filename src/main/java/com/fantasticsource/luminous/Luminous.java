eff520cb6bc13359155b6ef226ad934d0bb9e752
Laike-Endaril <silvertallon@gmail.com>
2025-03-20 14:53:29 -0700

    Updated library requirement

--- src/main/java/com/fantasticsource/luminous/Luminous.java
+++ src/main/java/com/fantasticsource/luminous/Luminous.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import static com.fantasticsource.luminous.asm.LuminousCore.*;
 
-@Mod(modid = MODID, name = NAME, version = VERSION, dependencies = "required-after:fantasticlib@[1.12.2.036y,);required-after:" + MODID + "core@[" + VERSION + ",)")
+@Mod(modid = MODID, name = NAME, version = VERSION, dependencies = "required-after:fantasticlib@[1.12.2.051,);required-after:" + MODID + "core@[" + VERSION + ",)")
 public class Luminous
 {
     @Mod.EventHandler
